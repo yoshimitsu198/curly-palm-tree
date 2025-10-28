184040c6e2a20523b8a2caf80310b1d8fad0b39c
yoshimitsu198 <yoshimitsu198@users.noreply.github.com>
2025-10-28 03:24:53 +0200

    test: fix memory leak

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// Updated iteration 18
+function func18(): boolean {
+    return true;
+}
+
+function processData18(data: string): string | null {
+    if (data) {
+        return data.toUpperCase();
+    }
+    return null;
+}
